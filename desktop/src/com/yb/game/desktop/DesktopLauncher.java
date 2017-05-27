66dc09ff16fe074091e8d487af618689a005a8cc
Barış Gündüzer <barisgunduzer@gmail.com>
2017-05-27 20:32:40 +0300

    Initial Commit

---
+++ desktop/src/com/yb/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.yb.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.yb.game.EmojiTetris;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EmojiTetris(), config);
+	}
+}
---
+++ html/src/com/yb/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.yb.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.yb.game.EmojiTetris;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new EmojiTetris();
+        }
+}
